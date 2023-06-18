0df315246cdd21d24432aa829516448d39e4fcbd
luccasst <lucas.c.r.f@hotmail.com>
2023-06-18 14:24:30 -0300

    add fetch in createAdminUser

---
+++ frontend/src/helpers/adminCreaterUser.js
@@ -0,0 +1,16 @@
+const adminCreateUser = async (body) => {
+    const storage = localStorage.getItem('admin');
+    const admin = JSON.parse(storage);
+    const options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json', authorization: admin.token },
+      body: JSON.stringify(body),
+    };
+    const url = 'http://localhost:3001/administrator';
+    const response = await fetch(url, options);
+    const data = response.json();
+    return data;
+  };
+  
+  export default adminCreateUser;
+  
