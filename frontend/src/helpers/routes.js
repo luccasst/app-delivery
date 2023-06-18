a0000d76ed576cc8a2dd7707ec8fadd79e086ce2
luccasst <lucas.c.r.f@hotmail.com>
2023-06-18 15:17:01 -0300

    add route in customerOrderPage

--- frontend/src/helpers/routes.js
+++ frontend/src/helpers/routes.js
@@ -4,6 +4,7 @@ import LoginPage from '../pages/loginPage/loginPage';
 import RegisterPage from '../pages/registerPage/registerPage';
 import RegisterManageUsers from '../pages/registerManageUsers/registerManageUsers';
 import CustomerProducts from '../pages/CustomerProducts/customerProducts';
+import CustomerOrder from '../pages/customerOrderPage/customerOrderPage';
 import SellerPage from '../pages/sellerOrders/sellerOrdersPage';
 import SellerOrdersById from '../pages/sellerOrdersById/sellerOrdersById';
 import CustomerOrdersById from '../pages/customerOdersById/customerOrdersById';
@@ -18,6 +19,7 @@ function Routes() {
         <Route exact path="/register" component={ RegisterPage } />
         <Route exact path="/customer/products" component={ CustomerProducts } />
         <Route exact path="/customer/orders/:id" component={ CustomerOrdersById } />
+        <Route exact path="/customer/orders" component={ CustomerOrder } />
         <Route exact path="/admin/manage" component={ RegisterManageUsers } />
         <Route exact path="/seller/orders" component={ SellerPage } />
         <Route exact path="/seller/orders/:id" component={ SellerOrdersById } />
