339f27752b2e047f74f23a1abc02279135640baf
luccasst <lucas.c.r.f@hotmail.com>
2023-06-18 15:30:01 -0300

    add route customer checkout

--- frontend/src/helpers/routes.js
+++ frontend/src/helpers/routes.js
@@ -5,6 +5,7 @@ import RegisterPage from '../pages/registerPage/registerPage';
 import RegisterManageUsers from '../pages/registerManageUsers/registerManageUsers';
 import CustomerProducts from '../pages/CustomerProducts/customerProducts';
 import CustomerOrder from '../pages/customerOrderPage/customerOrderPage';
+import CustomerCheckout from '../pages/CustomerCheckout/customerCheckout';
 import SellerPage from '../pages/sellerOrders/sellerOrdersPage';
 import SellerOrdersById from '../pages/sellerOrdersById/sellerOrdersById';
 import CustomerOrdersById from '../pages/customerOdersById/customerOrdersById';
@@ -20,6 +21,7 @@ function Routes() {
         <Route exact path="/customer/products" component={ CustomerProducts } />
         <Route exact path="/customer/orders/:id" component={ CustomerOrdersById } />
         <Route exact path="/customer/orders" component={ CustomerOrder } />
+        <Route exact path="/customer/checkout" component={ CustomerCheckout } />
         <Route exact path="/admin/manage" component={ RegisterManageUsers } />
         <Route exact path="/seller/orders" component={ SellerPage } />
         <Route exact path="/seller/orders/:id" component={ SellerOrdersById } />
